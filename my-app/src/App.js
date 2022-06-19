3f0edde6061b4b3d79aebf04c0d8ffbea0c2f75c
Siddhi_Hagawane <siddhihagawane16@gmail.com>
2022-06-19 16:06:10 +0530

    updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './Components/admin.css';
 import Header1 from './Components/Header1';
 import img from './Image/Walchand-logo.png'
 import Body from './Components/Body';
@@ -10,8 +11,28 @@ function App() {
     <>
     <Header1></Header1>
     <hr />
-    <body></body>
-    
+    <admin></admin>
+    <div>
+            <>
+            <div className="main">
+            <div className="sub-main">
+                <div>
+                  <h6>Sign in</h6>
+                    <div>
+                    <h6>Username*</h6>
+                      <input type="text" placeholder="user name" className="name"></input>
+                    </div>
+                    <div className='pass'>
+                      <input type="text" placeholder="password" className="name"></input>
+                    </div>
+                    <button>Log in</button>
+                </div>
+            </div>
+
+        </div>
+            </>
+            
+        </div>
     </>
      
   );
